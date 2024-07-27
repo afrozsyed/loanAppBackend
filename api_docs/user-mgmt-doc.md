c522512242a19b9d836bba67a7291178b5aad32e
afrozsyed <afrozsyed350@gmail.com>
2024-07-27 16:11:43 +0530

    User Registration api creation

--- api_docs/user-mgmt-doc.md
+++ api_docs/user-mgmt-doc.md
@@ -8,14 +8,40 @@ URI:: /api/v1/user-mgmt/register
 
 ### sample request
 ```
-
+{
+    "userName" : "test2",
+    "password" : "testPas1234",
+    "fullName" : "test user new",
+    "phoneNumber" : "985647",
+    "role" : "user" // optional ["user", "admin"]
+}
 ```
 ### sample success response
 ```
-
+{
+    "status": {
+        "code": 200,
+        "message": "success",
+        "type": "success"
+    },
+    "data": {
+        "_id": "66a4b558150ccb320de42ecf",
+        "userName": "test2",
+        "fullName": "test user new",
+        "phoneNumber": "985647",
+        "role": "user",
+        "createdAt": "2024-07-27T08:52:40.940Z",
+        "updatedAt": "2024-07-27T08:52:40.940Z",
+        "__v": 0
+    }
+}
 ```
 
 ### sample failure response
 ```
-
+{
+    "statusCode": 400,
+    "message": "User already exist",
+    "type": "error"
+}
 ```
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,59 @@
 import asyncHandler from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { isNullOrEmpty } from "../utils/validationUtil.js";
+import { User } from "../models/user.model.js";
 
 // controller method to register user
-const registerUser = asyncHandler( async(req,res) => {
-res.status(200).json({message: "Hello i am post of register"})
-})
+const registerUser = asyncHandler(async (req, res) => {
+  // take data from the request
+  const { userName, password, fullName, phoneNumber, role } = req.body;
+  console.log("==== req.body ==", req.body);
 
-export {registerUser}
+  // validate if all the required fileds are present and correct
+  if (
+    [fullName, userName, password, phoneNumber].some((field) => {
+      return isNullOrEmpty(field);
+    })
+  ) {
+    throw new ApiError(400, "mandatory fields are missing");
+  }
+
+  // check if user already exist
+  const existedUser = await User.findOne({ userName });
+  if (existedUser) {
+    console.log("method inside user already exist");
+    return res.status(400).json(new ApiError(400, "User already exist"));
+    //throw new ApiError(400, "User already exist");
+  }
+
+  // save the user to the database -- (create user object -- create entry in db)
+
+  const createdUser = await User.create({
+    userName,
+    password,
+    fullName,
+    phoneNumber,
+    role,
+  });
+
+  // check if user is created successfully
+  console.log("==== Created user ==", createdUser);
+  if (isNullOrEmpty(createdUser)) {
+    return res
+      .status(500)
+      .json(
+        new ApiError(400, "Something went wrong while registering the user")
+      );
+  }
+
+  // remove password and refresh tokens from the response object
+  const finalUserDetails = await User.findById(createdUser._id).select(
+    "-password -refreshToken"
+  );
+  console.log("==== finalUserDetails ==", finalUserDetails);
+  // send the response back to the client with the user details
+  res.status(200).json(new ApiResponse(200, "success", finalUserDetails));
+});
+
+export { registerUser };
